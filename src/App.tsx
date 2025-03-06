d86d15bf40746d40150285bec4fe080cda29e287
emo-todorov <emiliyantodorov89@gmail.com>
2025-03-06 18:48:57 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material"
+
+function App() {
+
+  return (
+    <Box sx={{ width: 400, margin: "auto", mt: 5 }}>
+      <h1>Register</h1>
+    </Box>
+  )
+}
+
+export default App
